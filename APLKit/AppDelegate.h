b0948ea3f26380d93e5c6d2e61fbeab5705a8768
王兴明 <applepeel@yeah.net>
2019-04-19 23:01:10 +0800

    init project

---
+++ APLKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APLKit
+//
+//  Created by 苹果皮 on 2019/4/19.
+//  Copyright © 2019年 AiJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APLKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  APLKit
+//
+//  Created by 苹果皮 on 2019/4/19.
+//  Copyright © 2019年 AiJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
